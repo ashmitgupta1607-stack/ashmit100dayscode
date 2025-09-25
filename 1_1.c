da3c7ba10e200ab48d1b46d04b09820c959f7e8a
ashmitgupta1607-stack <ashmitgupta1607@gmail.com>
2025-09-25 21:06:33 +0530

    Create 1_1.c

---
+++ 1_1.c
@@ -0,0 +1,12 @@
+//Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two value= ");
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("sum of numbers is = %d",sum);
+    return 0;
+}
